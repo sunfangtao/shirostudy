3c8d1d8af6416a698109f097efb8879f6df3c096
sunfangtao <sunfangtao@aioute.com>
2017-08-25 17:52:00 +0800

    数据库角色权限配置，以及项目整合

--- src/main/java/com/aioute/controller/LoginController.java
+++ src/main/java/com/aioute/controller/LoginController.java
@@ -1,12 +1,14 @@
 package com.aioute.controller;
 
 import com.aioute.controller.base.BaseController;
-import com.aioute.util.HttpClient;
-import org.apache.shiro.SecurityUtils;
+import com.sft.model.Permission;
+import com.sft.model.Role;
+import com.sft.service.RolePermissionService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,17 +16,51 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("loginController")
 public class LoginController extends BaseController {
 
+    @Resource
+    private RolePermissionService rolePermissionService;
+
     /**
      * 用户登录
      *
      * @param req
      * @param res
      */
-    @RequiresPermissions("sss")
     @RequestMapping("login")
     public void login(HttpServletRequest req, HttpServletResponse res) {
-        String uername = (String) SecurityUtils.getSubject().getPrincipal();
-        new HttpClient(req, res).send("http://www.baidu.com");
+        Role role = new Role();
+        role.setName("超级管理员");
+        role.setRemarks("没有备注");
+        role.setCreate_by("111");
+        boolean i = rolePermissionService.addRole(role);
+        role.setName("普通管理员");
+        role.setRemarks("没有备注");
+        role.setCreate_by("111");
+        rolePermissionService.addRole(role);
+        role.setName("用户");
+        role.setRemarks("没有备注");
+        role.setCreate_by("111");
+        rolePermissionService.addRole(role);
+
+        Permission permission = new Permission();
+        permission.setName("更新");
+        permission.setPermission("share:update");
+        permission.setServer_id("1");
+        permission.setCreate_by("111");
+        boolean j = rolePermissionService.addPermission(permission);
+        permission = new Permission();
+        permission.setName("添加");
+        permission.setPermission("share:add");
+        permission.setServer_id("1");
+        permission.setCreate_by("111");
+        rolePermissionService.addPermission(permission);
+        permission = new Permission();
+        permission.setName("删除");
+        permission.setPermission("share:delete");
+        permission.setServer_id("1");
+        permission.setCreate_by("111");
+        rolePermissionService.addPermission(permission);
+
+
     }
 
     /**
