2bd270fc0828c64c300d56aeadd0f4e8a1b008f0
sunfangtao <sunfangtao@aioute.com>
2017-09-04 17:52:44 +0800

    添加layui

--- src/main/java/com/aioute/controller/LoginController.java
+++ src/main/java/com/aioute/controller/LoginController.java
@@ -1,43 +1,18 @@
 package com.aioute.controller;
 
-import com.sft.service.FilterChainDefinitionsService;
-import com.sft.service.RolePermissionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("loginController")
 public class LoginController {
 
-    @Resource
-    private RolePermissionService rolePermissionService;
-    @Resource
-    private FilterChainDefinitionsService filterChainDefinitionsService;
-
     /**
      * 用户登录
-     *
-     * @param req
-     * @param res
      */
     @RequestMapping("login")
-    public void login(HttpServletRequest req, HttpServletResponse res) {
-        filterChainDefinitionsService.reloadFilterChains();
-    }
-
-    /**
-     * 用户登录
-     *
-     * @param req
-     * @param res
-     */
-    @RequestMapping("login2")
-    public void login2(HttpServletRequest req, HttpServletResponse res) {
-        int i = 0;
+    public String login2() {
+        return "redirect:/index.jsp";
     }
 
 }
